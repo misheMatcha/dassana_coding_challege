e762676579075bcce627b82cd72a772a3e9bfb77
misheMatcha <mishe.vong@gmail.com>
2021-02-09 01:20:30 -0800

    Setup template for results on home

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Item } from 'semantic-ui-react';
 import { fetchData } from '../util/api_util';
 
 const Home = () => {
@@ -8,16 +10,24 @@ const Home = () => {
     // limit results to 25 due to daily limitations on get requests
     // hard code query as its the home page
     // and pull items only as it's the array we need
-    fetchData(25, 'genshin lo-fi').then(data => setVideoList(data.items));
+    fetchData(5, 'lo-fi').then(data => setVideoList(data.items));
   }, []);
 
   const displayVideos = () => {
     return <ul>
       {
         videoList.map(video => {
-          return <li key={video.id.videoId}>
-            <p>{video.snippet.title}</p>
-          </li>
+          return <Item.Group as={Link} to={`/video/${video.id.videoId}`} key={video.id.videoId}>
+            <Item>
+              <Item.Image size='medium' src={video.snippet.thumbnails.medium.url} />
+              <Item.Content verticalAlign='middle'>
+                <Item.Header as='a'>{video.snippet.title}</Item.Header>
+                <Item.Description>
+                  {video.snippet.channelTitle}
+                </Item.Description>
+              </Item.Content>
+            </Item>
+          </Item.Group>
         })
       }
     </ul>

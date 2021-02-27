d7463fc41636951037c8e75e1cb1a84780ec3852
misheMatcha <mishe.vong@gmail.com>
2021-02-26 22:00:33 -0800

    remove uneeded files. install typescript. create tsx for App

---
+++ client/src/AppTS.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+// rename file after converstion to typescript
+
+const App: FC = () => {
+    return <div>
+        ts test
+    </div>
+}
+
+export default App;
